a990d3c6bee48352cfd20bbc0ec06cb543508a9c
Barbara Wahl <wahl@hanover.edu>
2015-03-02 17:08:04 -0500

    Add Loader's binomProb

--- panthrMath/specialfunc.js
+++ panthrMath/specialfunc.js
@@ -1,7 +1,7 @@
 (function(define) {'use strict';
 define(function(require) {
 
-   var C, lgamma, lfactorial, lchoose;
+   var C, lgamma, lfactorial, lchoose, lbinomProb, stirlerr;
 
    C = require('./constants');
 
@@ -37,10 +37,67 @@ define(function(require) {
       return lfactorial(n) - lfactorial(k) - lfactorial(n - k);
    };
 
+   // error term in Stirling's approximation
+   // log(n!) - log( sqrt(2*pi*n) * (n/e)^n )
+   stirlerr = (function() {
+      var cs, precomputed, N;
+      // coefficients in Stirling's expansion for log(Gamma)
+      cs = [1 / 12, 1 / 360, 1 / 1260, 1 / 1680, 1 / 1188];
+      precomputed = [NaN];
+      function compute(n) {
+         var nsq = n * n;
+         return cs[0] - (cs[1] - (cs[2] - (cs[3] - cs[4] / nsq) / nsq) / nsq ) / n;
+      }
+      for (N = 1; N < 16; N += 1) {
+         precomputed[N] = compute(N);
+      }
+      return function(n) { return precomputed[n] || compute(n); };
+   }());
+
+   // returns the log of the binomial probability
+   // Note: the arguments are re-arranged:  lbinomProb(n, p, x)
+   // calculates log(binom_prob(x; n, p)) where n > 0 and
+   // 0 <= p <= 1.
+   // Based on:  "Fast and Accurate Computation of Binomial Probabilities",
+   // Loader (2000)
+   lbinomProb = (function() {
+      function dev(np, x) {
+         var ej, j, s, sTemp, v;
+         if (Math.abs(x - np) < 0.1 * (x + np)) {
+            v = (x - np) / (x + np);
+            s = (x - np) * v;
+            ej = 2 * x * v;
+            for (j = 1; j < 10000; j += 1) {
+               ej *= v * v;
+               sTemp = s + ej / (2 * j + 1);
+               if (sTemp === s) {
+                  return sTemp;
+               }
+               s = sTemp;
+            }
+            throw new Error('dev(np, x) made it to 10000 ...');
+         }
+         return x * Math.log(x / np) + np - x;
+      }
+
+      return function(n, p, x) {
+         if (p === 0) { return x === 0 ? 0 : -Infinity; }
+         if (p === 1) { return x === n ? 0 : -Infinity; }
+         if (x === 0) { return n * Math.log(1 - p); }
+         if (x === n) { return n * Math.log(p); }
+         return stirlerr(n) - stirlerr(x) - stirlerr(n - x) -
+            dev(n * p, x) - dev(n * (1 - p), n - x) +
+            0.5 * Math.log(n / (C.twopi * x * (n - x)));
+      };
+   }());
+
+
    return {
       lgamma: lgamma,
       lfactorial: lfactorial,
-      lchoose: lchoose
+      lchoose: lchoose,
+      lbinomProb: lbinomProb,
+      stirlerr: stirlerr
    };
 
 });
