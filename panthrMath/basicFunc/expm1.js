24ccb5edb1c81da07de44004179317e3f511ae4d
Barbara Wahl <wahl@hanover.edu>
2015-07-29 15:28:26 -0400

    Manually implement expm1. Close #12.

--- panthrMath/basicFunc/expm1.js
+++ panthrMath/basicFunc/expm1.js
@@ -2,12 +2,34 @@
 define(function(require) {
 
    /**
-    * Implementation / polyfill for exp(x) - 1
+    * exp(x) - 1
+    * Taken from:  Computation of the Incomplete Gamma Function Ratios
+    * and their Inverse, by DiDonato and Morris.
     */
+   var Rational, expm1;
 
-   return {
-      expm1: Math.expm1 || function(x) { return Math.exp(x) - 1; }
-   };
+   Rational = require('../rational');
+
+   expm1 = (function() {
+      var R = new Rational([
+          0.0238082361044469,
+          0.914041914819518e-9,
+          1
+         ], [
+          0.595130811860248e-3,
+         -0.0119041179760821,
+          0.107141568980644,
+         -0.499999999085958,
+          1
+         ]);
+      return function(x) {
+         if (x < -0.15) { return Math.exp(x) - 1; }
+         if (x > 0.15) { return Math.exp(x) * (1 - 1 / Math.exp(x)); }
+         return x * R.evalAt(x);
+      };
+   }());
+
+   return { expm1: expm1 };
 
 });  // end define
 
---
+++ test/expm1.spec.js
@@ -0,0 +1,44 @@
+var chai = require('chai');
+var expect = chai.expect;
+var precision = 1e-10;
+var expm1 = require('../panthrMath/basicFunc/expm1').expm1;
+
+chai.use(function(_chai, utils) {
+   var Assertion = _chai.Assertion;
+   Assertion.addMethod('relativelyCloseTo', function(x0, delta) {
+      var x = utils.flag(this, 'object');
+      var denom = Math.max(Math.abs(x0), Math.abs(x));
+      if (denom !== 0) {
+         var res = Math.abs(x - x0) / denom;
+         new Assertion(res).to.be.below(delta);
+      }
+   });
+});
+
+describe('expm1 function', function() {
+   it('works', function() {
+      [[-0.1,-0.0951625819640404],
+       [-0.01,-0.00995016625083195],
+       [-0.001,-0.000999500166625008],
+       [-1e-04,-9.99950001666625e-05],
+       [-1e-05,-9.99995000016667e-06],
+       [-1e-06,-9.99999500000167e-07],
+       [-1e-07,-9.99999950000002e-08],
+       [0.1,0.105170918075648],
+       [0.01,0.0100501670841681],
+       [0.001,0.00100050016670834],
+       [1e-04,0.000100005000166671],
+       [1e-05,1.00000500001667e-05],
+       [1e-06,1.00000050000017e-06],
+       [1e-07,1.00000005e-07],
+       [1,1.71828182845905],
+       [2,6.38905609893065],
+       [3,19.0855369231877],
+       [4,53.5981500331442],
+       [5,147.413159102577]
+      ].forEach(function(pair) {
+      expect(expm1(pair[0]))
+         .to.be.relativelyCloseTo(pair[1], precision);
+      });
+   });
+});
