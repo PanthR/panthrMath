e30e327d4f6ab9727590c2a9012b0a91829755da
Barbara Wahl <wahl@hanover.edu>
2015-07-31 12:41:42 -0400

    Implement repeat method.

--- panthrMath/utils.js
+++ panthrMath/utils.js
@@ -1,6 +1,29 @@
 (function(define) {'use strict';
 define(function(require) {
 
+   /* repeat, for carrying out repeated improvement until stopping
+    * condition is satisfied.
+    *
+    * `init` is the initial value,
+    * `step` is a function which returns the new value, and
+    * `n` is a non-negative integer (number of steps to take),
+    *  a function returning a boolean, or missing.
+    */
+   function repeat(init, step, n) {
+      var prev, curr, done;
+      curr = init;
+      if (n === 0) { return curr; }
+      done = typeof n === 'function' ? n :
+             n > 0 ? function() { n -= 1; return n === 0; } :
+                     function() { return curr === prev; };
+      while (!done()) {
+         prev = curr;
+         curr = step();
+      }
+      return curr;
+   }
+
+   /* mixin */
    return {
       mixin: function mixin(target) {
          Array.prototype.slice.call(arguments, 1)
@@ -10,7 +33,8 @@ define(function(require) {
                });
             });
          return target;
-      }
+      },
+      repeat: repeat
    };
 
 });
