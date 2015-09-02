171603be2860ebefc61ab426f169cdcf86ffb6bb
Haris Skiadas <cskiadas@gmail.com>
2015-09-02 10:54:08 -0400

    Update bratio. Ref #9

--- panthrMath/basicFunc/bratio.js
+++ panthrMath/basicFunc/bratio.js
@@ -10,15 +10,21 @@ define(function(require) {
     * Based on "Significant Digit Computation of the Incomplete Beta Function Ratios",
     * DiDonato, Morris
     */
-   var C, stirlerr, lgamma, algdiv, series;
+   var C, stirlerr, log1p, gam1, algdiv, series, logspaceAdd,
+       lgamma, lbeta, gratio;
 
    C = require('../constants');
-   stirlerr = require('../basicfunc/stirlerr').stirlerr;
-   lgamma = require('../basicfunc/lgamma').lgamma;
+   stirlerr = require('./stirlerr').stirlerr;
+   log1p = require('./log1p').log1p;
+   gam1 = require('./gam1').gam1;
+   lgamma = require('./lgamma').lgamma;
+   lbeta = require('./lbeta').lbeta;
    series = require('../utils').series;
+   logspaceAdd = require('../utils').logspaceAdd;
+   gratio = require('./gratio').gratio;
 
    // Page 371 of DiDonato/Morris
-   // Computes lgamma(b) - lgamma(a + b)
+   // algdiv(a, b) = lgamma(b) - lgamma(a + b)
    algdiv = (function(){
       var ds;
       ds = [
@@ -52,19 +58,84 @@ define(function(require) {
              stirlerr(b) + algdiv(a, a + b);
    }
 
-   // bpser (7)
-   function bpser(a, b, x) {
+   // Assumes lp is the log of the lower prob
+   // Returns a pair lower/upper in log space
+   // fromUpper assumes given upper prob in log space
+   function fromLower(lp) {
+      return { lower: lp, upper: log1p(-Math.exp(lp)) };
+   }
+   function fromLower(lq) {
+      return { lower: log1p(-Math.exp(lq)), upper: lq };
+   }
 
+   // bpser (7) Returns the log of the lower probability
+   function bpser(a, b, x) {
+      var prob;
+
+      return -lbeta(a, b) + a * Math.log(x) - Math.log(a) +
+         log1p(a * series(function(i, v) {
+            if (i === 0) { return 0; }
+            if (i === 1) { return (1 - b) * x / (a + 1); }
+            return v * (i - b) * x * (a + i - 1) / i / (a + i);
+         }));
+         // TODO: Check for need of stopping condition
+   }
+
+   // bup (8)
+   // Assumes n >= 1
+   function bup(a, b, x, y, n) {
+      return a * Math.log(x) + b * Math.log(y) +
+         Math.log(series(function(i) {
+            return Math.pow(x, i) / Math.exp(lbeta(b, a + i)) / (a + i + 1);
+         }, n));
+      // TODO: Should it perhaps not return the log?
+   }
+
+   // bgrat (9)
+   // log space
+   // Assumes a > b
+   // Not using the "grat_r" version as in r-code.
+   function bgrat(a, b, x) {
+      var T, fTs, u, logH, logM, lnx2s, lnx2sn, ps, cs, j;
+
+      T = a + 0.5 * (b - 1);
+      fTs = 1 / (4 * T * T);
+      u = -T * Math.log(x);
+      logH = Math.log(b) + log1p(gam1(b)) + b * Math.log(u) - u;
+      logM = logH - (algdiv(b, a) + b * Math.log(T));
+      cs = [1];   // cs[n] = 1 / (2n + 1)!
+      ps = [1];   // ps[n] = p_n in (9.2)
+      lnx2s = Math.log(x) * 0.5;   // (ln(x) / 2) ^ 2
+      lnx2s = lnx2s * lnx2s;
+      lnx2sn = 1;   // lnx2s ^ n
+      j = gratio(b)(u) / Math.exp(logH);
+      return logM + Math.log(
+         series(function(n) {
+            var mbmn = -n; // holds m*b - n
+            if (n > 0) {
+               cs[n] = cs[n - 1] / (2 * n) / (2 * n + 1);
+               ps[n] = (b - 1) * cs[n] + 1 / n * series(function(m) {
+                  if (m === 0) { return 0; }
+                  mbmn = mbmn + b;
+                  return mbmn * cs[m] * ps[n - m];
+               }, n);
+            }
+            lnx2sn = lnx2sn * lnx2s;
+            j = (b + 2 * n) * (b + 2 * n + 1) * j + (u + b + 2 * n + 1) * lnx2sn;
+            j = j * fTs;
+            return ps[i] * j;
+         });
+      );
    }
 
    function bratioLog(a, b, x) {
-      var p, q, y, bint, bfrac;
+      var p, q, y, bint, bbar;
 
       p = a / (a + b);
       q = b / (a + b);
       y = 1 - x;
       bint = Math.floor(b);
-      bfrac = b - bint;
+      bbar = b - bint;
 
       // By convention, if 0 < x < 1 then a == 0 -> result = 1
       // and b == 0 -> result = 0. Can't have a == b == 0.
@@ -82,55 +153,64 @@ define(function(require) {
          // also skipping a < min(eps, b*eps)
          if ( Math.max(a, b) > 1) {
             // either a <= 1 < b or b <= 1 < a
-            if (b <= 1) { return bpser(a, b, x); } // (12c)
+            if (b <= 1) { return fromLower(bpser(a, b, x)); } // (12c)
             // a <= 1 < b
-            if (x >= 0.3) { return tailFlip(bpser(b, a, y)); } // (13b)
+            if (x >= 0.3) { return fromUpper(bpser(b, a, y)); } // (13b)
             // x < 0.3
-            if (x < 0.1 && Math.pow(x * b, a) <= 0.7) { return bpser(a, b, x); } // (12d)
+            if (x < 0.1 && Math.pow(x * b, a) <= 0.7) { return fromLower(bpser(a, b, x)); } // (12d)
             // x >= 0.1 or (bx)^a > 0.7
             // for now, assume bgrat will start with w0 = 0
-            if (b > 15) { return tailFlip(bgrat(b, a, y)); } // (14a, 14b)
+            if (b > 15) { return fromUpper(bgrat(b, a, y)); } // (14a, 14b)
             // b <= 15
-            // TODO: bgrat here is supposed to take a w=bup(b, a, 1 - x, 20??)
-            return tailFlip(bgrat(b + 20, a, y)); // (15a, 15b)
+            // TODO: bgrat here is supposed to take a w=bup(b, a, 1 - x, x, 20??)
+            return fromUpper(bgrat(b + 20, a, y)); // (15a, 15b)
          }
          // max(a, b) <= 1
-         if (a >= Math.min(0.2, b)) { return bpser(a, b, x); } // (12a)
+         if (a >= Math.min(0.2, b)) { return fromLower(bpser(a, b, x)); } // (12a)
          // a < min(0.2, b)
-         if (Math.pow(x, a) <= 0.9) { return bpser(a, b, x); } // (12b)
+         if (Math.pow(x, a) <= 0.9) { return fromLower(bpser(a, b, x)); } // (12b)
          // x^a > 0.9
-         if (x >= 0.3) { return tailFlip(bpser(b, a, y)); } // (13a)
+         if (x >= 0.3) { return fromUpper(bpser(b, a, y)); } // (13a)
          // x < 0.3
-         // TODO: bgrat here is supposed to take a w=bup(b, a, 1 - x, 20??)
-         return tailFlip(bgrat(b + 20, a, y));  // 15c
+         // TODO: bgrat here is supposed to take a w=bup(b, a, 1 - x, x, 20??)
+         return fromUpper(bgrat(b + 20, a, y));  // 15c
       }
       // min(a, b) > 1
       if (x > p) { return bratioLog(b, a, y); }
       if (b < 40) {
-         if (b * x <= 0.7) { return bpser(a, b, x); } // (16a)
+         if (b * x <= 0.7) { return fromLower(bpser(a, b, x)); } // (16a)
          // b * x > 0.7
-         if (x <= 0.7) { return bup(bfrac, a, y, bint) + bpser(a, bfrac, x); } // (17a)
+         if (x <= 0.7) {
+            return fromLower(logspaceAdd(bup(bbar, a, y, x, bint),
+                                         bpser(a, bbar, x)));
+         } // (17a)
          // x > 0.7
-         // TODO need w0=bup(bfrac, a, y, bint)
-         if (a > 15) { return bgrat(a, bfrac, x); } // (18a)
+         // TODO need w0=bup(bbar, a, y, bint)
+         if (a > 15) {
+            return fromLower(logspaceAdd(bup(bbar, a, y, x, bint),
+                                         bgrat(a, bbar, x)));
+         } // (18a)
          // a <= 15
-         // TODO: w0 = bup(bfrac, a, y, bint) + bup(a, bfrac, x, 20)
-         return bgrat(a + 20, bfrac, x); // (19a)
+         // TODO: w0 =
+         return fromLower(logspaceAdd(
+                              logspaceAdd(bup(bbar, a, y, x, bint),
+                                          bup(a, bbar, x, y, 20)),
+                              bgrat(a + 20, bbar, x))); // (19a)
       }
       // b >= 40
       if (a <= b) {
-         if (a <= 100) { return bfrac(a, b, x); } // (20a)
+         if (a <= 100) { return fromLower(bfrac(a, b, x)); } // (20a)
          // a > 100
-         if (x < 0.97 * p) { return bfrac(a, b, x); } // (20b)
+         if (x < 0.97 * p) { return fromLower(bfrac(a, b, x)); } // (20b)
          // x >= 0.97 * p
-         return basym(a, b, x); // (21a)
+         return fromLower(basym(a, b, x)); // (21a)
       }
       // a > b
-      if (b <= 100) { return bfrac(a, b, x); } // (20c)
+      if (b <= 100) { return fromLower(bfrac(a, b, x)); } // (20c)
       // b > 100
-      if (y > 1.03 * q) { return bfrac(a, b, x); } // (20d)
+      if (y > 1.03 * q) { return fromLower(bfrac(a, b, x)); } // (20d)
       // y <= 1.03 * q
-      return basym(a, b, x); // (21c)
+      return fromLower(basym(a, b, x)); // (21c)
    }
 
    /*
