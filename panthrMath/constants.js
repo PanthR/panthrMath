16e6dbeae0a566f743df18a3e9a360df8879db74
Barbara Wahl <wahl@hanover.edu>
2015-12-26 08:17:26 -0500

    Work on documentation of constants.

--- panthrMath/constants.js
+++ panthrMath/constants.js
@@ -3,25 +3,27 @@ define(function(require) {
    var C;
 
    /**
-    * Constants module
+    * Constants module, provides frozen values for some frequently-used constants.
     * @module C
     * @author Haris Skiadas <skiadas@hanover.edu>, Barb Wahl <wahl@hanover.edu>
     */
    C = {
       /**
-       * TODO
+       * $2\pi$
        */
       twopi: 2 * Math.PI,
       /**
-       * TODO
+       *$\sqrt{2\pi}$
        */
       sqrt2pi: Math.sqrt(2 * Math.PI),
       /**
-       * TODO
+       * $\ln(2\pi)$
        */
       log2pi: Math.log(2 * Math.PI),
       /**
-       * TODO
+       * The Euler-Mascheroni constant
+       * \\[\gamma = \lim \_ {n \to \infty} \left(-\ln(n) + \sum \_ {k=1}^n \frac{1}{k}\right) = 0.57721566 \ldots\\]
+       *
        */
       eulerGamma: 0.577215664901532860606512090082
    };
