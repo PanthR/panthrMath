3e26f1b9d714c2514de23f910f0dc3291fbea68b
Haris Skiadas <cskiadas@gmail.com>
2016-03-06 16:28:56 -0500

    Add Kolmogorov-Smirnov integration test for cont distributions. Ref #70

--- test/integration.spec.js
+++ test/integration.spec.js
@@ -13,6 +13,7 @@ var chisq = main.chisq;
 var precision = 1e-8;
 
 var n = 10000;    // Number of random variates generated for each test
+var nks = 1000;
 var reps = 100;    // Number of times to repeat test
 
 // Returns the array [a, a+1, ..., b]
@@ -83,6 +84,151 @@ function testDiscrete(distrObj, xs, nparams) {
       }, 0) / reps;
 }
 
+/*
+ * This will do a Kolmogorov-Smirnov goodness-of-fit test.
+ */
+function testCont(distrObj) {
+   var pvalues, rep, observed, ksdiffs, dstat, i;
+
+   pvalues = [];
+
+   for (rep = 0; rep < reps; rep += 1) {
+      observed = [];
+      for (i = 0; i < nks; i += 1) {
+         observed.push(distrObj.r());
+      }
+      observed.sort(function(x, y) { return x - y; });
+      ksdiffs = observed.map(function(x, i) {
+         return distrObj.p(x) - i / nks;
+      });
+      dstat = ksdiffs.reduce(function(acc, x) {
+         return Math.max(acc, Math.abs(x), Math.abs(1 / nks - x));
+      }, -1);
+
+      pvalues.push(ks(nks, dstat));
+      // ...  put pvalue in pvalues
+   }
+   // return pvalues;
+   return pvalues.reduce(function(acc, v) {
+      return v < 0.05 ? (acc + 1) : acc;
+   }, 0) / reps;
+
+}
+
+
+// BEGIN KS IMPLEMENTATION
+
+ /* Compute Kolmogorov's distribution.
+    Code published in
+ George Marsaglia and Wai Wan Tsang and Jingbo Wang (2003),
+ "Evaluating Kolmogorov's distribution".
+ Journal of Statistical Software, Volume 8, 2003, Issue 18.
+ URL: http://www.jstatsoft.org/v08/i18/.
+
+ We are returning the upper-tail p-value
+ */
+function ks(n, d)
+{
+   var k, m, i, j, g, h, s, H, Q, basicArr;
+
+   /*
+      The faster right-tail approximation is omitted here.
+   */
+   s = d * d * n;
+   if (s > 7.24 || (s > 3.76 && n > 99)) {
+      return 2 * Math.exp(-(2.000071+.331/Math.sqrt(n)+1.409/n)*s);
+   }
+   k = Math.floor(n * d) + 1;
+   m = 2 * k - 1;
+   h = k - n * d;
+   basicArr = seq(1, m * m);
+   H = basicArr.map(function() { return 0; });
+   Q = basicArr.map(function() { return 0; });
+   for (i = 0; i < m; i++) {
+       for (j = 0; j < m; j++) {
+         H[i * m + j] = i - j + 1 < 0 ? 0 : 1;
+       }
+   }
+   // Fixing "numerators"
+   for (i = 0; i < m; i++) {
+       H[i * m] -= Math.pow(h, i + 1);
+       H[(m - 1) * m + i] -= Math.pow(h, m - i);
+   }
+   // Adjust the bottom left corner if h > 1/2
+   H[(m - 1) * m] += 2 * h - 1 > 0 ? Math.pow(2 * h - 1, m) : 0;
+   // Fix "denominators"
+   // TODO: Maybe can do better?
+   for (i = 0; i < m; i++) {
+       for (j = 0; j < i + 1; j++) {
+            for (g = 1; g <= i - j + 1; g++) {
+               H[i * m + j] /= g;
+            }
+       }
+   }
+
+   // Q will be an object with "arr" and "exp" properties
+   Q = m_power({ arr: H, exp: 0 }, m, n);
+
+   s = Q.arr[(k - 1) * m + k - 1];
+   for(i = 1; i <= n; i++) {
+       s = s * i / n;
+       if (s < 1e-140) {
+         s *= 1e140;
+         Q.exp -= 140;
+       }
+   }
+   return 1 - s * Math.pow(10, Q.exp);
+}
+
+// Multiplication of two mxm matrices.
+function m_multiply(A, B, m) {
+   var i, j, k, C;
+   C = [];
+   for (i = 0; i < m; i++) {
+      for (j = 0; j < m; j++) {
+         C[i * m + j] = 0;
+         for(k = 0; k < m; k++) {
+            C[i * m + j] += A[i * m + k] * B[k * m + j];
+         }
+      }
+   }
+   return C;
+}
+
+// Computes the "n"-th power of an mxm matrix
+// arrObj is an object with properties arr and exp
+// Returns an object with the same properties.
+function m_power(arrObj, m, n) {
+
+   var halfObj, retObj, i;
+
+   if(n == 1) {
+      return arrObj;
+   }
+
+   halfObj = m_power(arrObj, m, Math.floor(n / 2));
+
+   retObj = { arr: m_multiply(halfObj.arr, halfObj.arr, m),
+              exp: 2 * halfObj.exp };
+
+   if (n % 2 == 1) {
+      retObj = { arr: m_multiply(arrObj.arr, retObj.arr, m),
+                 exp: arrObj.exp + retObj.exp };
+    }
+
+    if (retObj.arr[(m / 2) * m + (m / 2)] > 1e140) {
+      for(i = 0; i < m * m; i++) {
+         retObj.arr[i] *= 1e-140;
+      }
+      retObj.exp += 140;
+    }
+
+    return retObj;
+}
+
+// END KS IMPLEMENTATION
+
+
 describe("(slow) Integration test", function() {
    describe("for binomial", function() {
       seq(1, 20).forEach(function() {
@@ -125,6 +271,61 @@ describe("(slow) Integration test", function() {
          setObj.ws[3] = 0.43;
          dObj.r = main.finite(setObj).r;
          expect(testDiscrete(dObj, setObj.xs, 0)).to.be.above(0.2);
+      });
+   });
+   describe("for normal", function() {
+      it("repeated test", function() {
+         var mu = Math.random() * 5;
+         var sigma = 1 + Math.random() * 3;
+         var distr = main.normal(mu, sigma);
+         expect(testCont(distr)).to.be.below(0.2);
+         distr.r = main.normal(mu, 1.1 * sigma).r;
+         expect(testCont(distr)).to.be.above(0.2);
+         distr.r = main.normal(mu + 0.1 * sigma, sigma).r;
+         expect(testCont(distr)).to.be.above(0.2);
+      });
+   });
+   describe("for uniform", function() {
+      it("repeated test", function() {
+         var min = Math.random() * 10;
+         var max = min + Math.random() * 20;
+         var distr = main.unif(min, max);
+         expect(testCont(distr)).to.be.below(0.2);
+         distr.r = main.unif(min, 1.1 * max).r;
+         expect(testCont(distr)).to.be.above(0.2);
+         distr.r = main.unif(1.1 * min, max).r;
+         expect(testCont(distr)).to.be.above(0.2);
+      });
+   });
+   describe("for t", function() {
+      it("repeated test", function() {
+         var df = Math.random() * 30;
+         var distr = main.tdistr(df);
+         expect(testCont(distr)).to.be.below(0.2);
+      });
+   });
+   describe("for gamma", function() {
+      it("repeated test", function() {
+         var s = 1 + Math.random() * 4;
+         var a = 1 + Math.random() * 4;
+         var distr = main.gammadistr(a, s);
+         expect(testCont(distr)).to.be.below(0.2);
+         distr.r = main.gammadistr(a, 1.1 * s).r;
+         expect(testCont(distr)).to.be.above(0.2);
+         distr.r = main.gammadistr(1.1 * a, s).r;
+         expect(testCont(distr)).to.be.above(0.2);
+      });
+   });
+   describe.only("for beta", function() {
+      it("repeated test", function() {
+         var a = 1 + Math.random() * 6;
+         var b = 1 + Math.random() * 6;
+         var distr = main.betadistr(a, b);
+         expect(testCont(distr)).to.be.below(0.2);
+         distr.r = main.betadistr(a, 1.1 * b).r;
+         expect(testCont(distr)).to.be.above(0.2);
+         distr.r = main.betadistr(1.1 * a, b).r;
+         expect(testCont(distr)).to.be.above(0.2);
       });
    });
 });
